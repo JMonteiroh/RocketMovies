dddefe7e869d5b6710fd673b3e0ae523cf411644
João Paulo <joao.paulomonteiro@hotmail.com>
2023-03-04 08:08:11 -0300

    created routes

---
+++ src/styles/theme.js
@@ -0,0 +1,18 @@
+export default {
+  colors: {
+    pink: '#FF859B',
+    lightPurple: 'rgba(255, 133, 155, 0.05)', 
+    black: '#0D0C0F',
+    brown: '#282124',
+    gray_900: '#1C1B1E',
+    gray_800: '#262529',
+    gray_700: '#312E38',
+    gray_600: '#3E3B47',
+    gray_500: '#948F99',
+    gray_400: '#CAC4CF',
+    gray: '#E5E5E5',
+    ice: '#F4EDE8',
+    white: '#FFFFFF', 
+  }
+  
+}
